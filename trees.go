a883f146513fd49bda4a0947190f244578ecbecb
grbll@posteo.com <grbll@posteo.com>
2025-03-17 17:11:12 +0100

    design of arbitrary tree interface

--- trees.go
+++ trees.go
@@ -1,6 +1,19 @@
 package trees
 
-type ArbitraryTree interface {
-	Parent() *ArbitraryTree
-	Children() []*ArbitraryTree
+type Tree interface {
+	Parent() *Tree
+	Children() []*Tree
+}
+
+func Traversal[T Tree](tree T, activation func(T, int)) {
+	var path []int = []int{0}
+	var direction int = 0
+	for len(path) != 0 {
+		activation(tree, direction)
+		if direction < len(tree.Children()) {
+			path = append(path, direction)
+			tree = tree.Children()[direction]
+			direction = 0
+		}
+	}
 }
