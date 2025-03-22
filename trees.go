341f64fd5827489339abb44869db14efe884c1d0
grbll@posteo.com <grbll@posteo.com>
2025-03-22 17:30:33 +0100

    typing for synchronous trees

--- trees.go
+++ trees.go
@@ -1,19 +1,24 @@
 package trees
 
-type Tree interface {
-	Parent() *Tree
-	Children() []*Tree
+type Tree[T any] interface {
+	Parent() Tree[T]
+	Children() []Tree[T]
 }
 
-func Traversal[T Tree](tree T, activation func(T, int)) {
-	var path []int = []int{0}
-	var direction int = 0
-	for len(path) != 0 {
-		activation(tree, direction)
-		if direction < len(tree.Children()) {
-			path = append(path, direction)
-			tree = tree.Children()[direction]
-			direction = 0
+func Traversal[B any, T Tree[B]](tree T, activation func(T, int)) {
+	var currentNode Tree[B] = tree
+	if currentNode != nil {
+		var direction int = 0
+		var path []int = []int{0}
+		for len(path) > 0 {
+			activation(tree, direction)
+			if direction < len(currentNode.Children()) {
+				path = append(path, direction)
+				direction = 0
+			} else {
+				direction = path[len(path)-1]
+				path = path[:len(path)-1]
+			}
 		}
 	}
 }
